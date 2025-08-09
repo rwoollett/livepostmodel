71a9452f5d8e5185a8f67484fbd689b0d784eac3
Rodney Woollett <rodney.woollett@gmail.com>
2025-08-09 20:23:44 +1200

    Date is optional

--- livepostsmodel/LivePosts.cpp
+++ livepostsmodel/LivePosts.cpp
@@ -5,7 +5,7 @@ namespace LivePostsModel
 
   bool Validate::Post(const LivePostsModel::Post &o)
   {
-    return !(o.title.empty() || o.content.empty() || o.user.empty() || o.date.empty());
+    return !(o.title.empty() || o.content.empty() || o.user.empty());
   }
 
 }
